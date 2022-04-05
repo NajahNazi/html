02df5046181fe42e4ef8bc54920c53c98d6e8135
Najahu Naziratu <101425090+NajahNazi@users.noreply.github.com>
2022-04-05 09:52:18 -0700

    my shop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 import {Routes, Route} from "react-router-dom"
+import Product from './components/Product'
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <Routes>
-    <Route path="/" element={<Home/>}/>
+    
+    <Route path='/' element={ <Home/>}/>
+    <Route path='/product' element={ <Product/>}/>
+   
     </Routes>
+    </>
   )
 }
 
